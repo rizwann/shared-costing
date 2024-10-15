909c31a0f14170af14dcc9bd4214ddee643ad7bf
Rizwan Kabir <rizwankabirsizan@gmail.com>
2024-10-15 22:24:12 +0200

    add furniture cat

--- src/models/Expense.ts
+++ src/models/Expense.ts
@@ -23,6 +23,7 @@ export enum CategoryName {
   Butcher = "Butcher",
   Travel = "Travel",
   Electronics = "Electronics",
+  Furniture = "Furniture",
   Utilities = "Utilities",
   Health = "Health",
 }
