c5753fef1029d741cb2263ed99a9ce3356d215ed
Rizwan Kabir <rizwankabirsizan@gmail.com>
2025-04-18 17:43:14 +0200

    add field and bowling data

--- src/controllers/playerController.ts
+++ src/controllers/playerController.ts
@@ -46,41 +46,133 @@ export async function scrapeMembers(req: Request, res: Response) {
     
   
        // now go to each player profile page and extract the stats one by one without closing the browser instance
-      for (let i = 0; i < membersWithProfileLink.length; i++) {
-        await page.goto(membersWithProfileLink[i].profileLink, { waitUntil: 'networkidle2', timeout: 0 });
-        const playerStats = await page.evaluate(() => {
+       for (let i = 0; i < membersWithProfileLink.length; i++) {
+        try {
+          await page.goto(membersWithProfileLink[i].profileLink, { waitUntil: 'networkidle2', timeout: 0 });
+      
+          // Wait for batting stats
+          await page.waitForSelector('.statWrapper', { timeout: 10000 });
+      
+          // Extract Batting Stats
+          const battingStats = await page.evaluate(() => {
             const stats = Array.from(document.querySelectorAll('.statWrapper')).map(stat => {
-                const statValue = (stat.querySelector('.stat') as HTMLElement).innerText;
-                const statName = (stat.querySelector('.statName') as HTMLElement).innerText;
-                return {
-                    [statName]: statValue,
-                };
+              const statValue = (stat.querySelector('.stat') as HTMLElement)?.innerText || '';
+              const statName = (stat.querySelector('.statName') as HTMLElement)?.innerText || '';
+              return { [statName]: statValue };
             });
             return stats;
-        });
-       
+          });
+      
+          // Try clicking on Bowling tab
+          const bowlingTabExists = await page.evaluate(() => {
+            const tab = document.querySelector('[value="bowling"]') as HTMLElement;
+            if (tab) {
+              tab.click();
+              return true;
+            }
+            return false;
+          });
+      
+          let bowlingStats
+      
+          if (bowlingTabExists) {
+            try {
+              await page.waitForSelector('.statWrapper', { timeout: 10000 });
+              bowlingStats = await page.evaluate(() => {
+                const stats = Array.from(document.querySelectorAll('.statWrapper')).map(stat => {
+                  const statValue = (stat.querySelector('.stat') as HTMLElement)?.innerText || '';
+                  const statName = (stat.querySelector('.statName') as HTMLElement)?.innerText || '';
+                  return { [statName]: statValue };
+                });
+                return stats;
+              });
+            } catch (err) {
+              console.warn(`Bowling stats not found for player ${membersWithProfileLink[i].name}`);
+            }
+          } else {
+            console.warn(`Bowling tab not available for player ${membersWithProfileLink[i].name}`);
+          }
 
-        const player = {
+      // / Try clicking on Fielding tab
+          const fieldingTabExists = await page.evaluate(() => {
+            const tab = document.querySelector('[value="fielding"]') as HTMLElement;
+            if (tab) {
+              tab.click();
+              return true;
+            } 
+            return false;
+          }
+          );
+          let fieldingStats
+          if (fieldingTabExists) {
+            try {
+              await page.waitForSelector('.statWrapper', { timeout: 10000 });
+              fieldingStats = await page.evaluate(() => {
+                const stats = Array.from(document.querySelectorAll('.statWrapper')).map(stat => {
+                  const statValue = (stat.querySelector('.stat') as HTMLElement)?.innerText || '';
+                  const statName = (stat.querySelector('.statName') as HTMLElement)?.innerText || '';
+                  return { [statName]: statValue };
+                });
+                return stats;
+              });
+            } catch (err) {
+              console.warn(`Fielding stats not found for player ${membersWithProfileLink[i].name}`);
+            }
+          } else {
+            console.warn(`Fielding tab not available for player ${membersWithProfileLink[i].name}`);
+          }
+
+          const player = {
             ...membersWithProfileLink[i],
-            stats: playerStats.reduce((acc, stat) => ({ ...acc, ...stat }), {}),
-        }
-
-       // save to database, and if player already exists, update the stats
-        const existingPlayer = await PlayerStat.findOne({ player_id: player.player_id });
-        if (existingPlayer) {
+            stats: {
+              batting: battingStats ? battingStats.reduce((acc: any, stat: any) => {
+                const key = Object.keys(stat)[0];
+                acc[key] = stat[key];
+                return acc;
+              }
+              , {}) : null,
+              bowling: bowlingStats ? bowlingStats.reduce((acc: any, stat: any) => {
+                const key = Object.keys(stat)[0];
+                acc[key] = stat[key];
+                return acc;
+              }, {}) : null,
+              fielding: fieldingStats ? fieldingStats.reduce((acc: any, stat: any) => {
+                const key = Object.keys(stat)[0];
+                acc[key] = stat[key];
+                return acc;
+              }, {}) : null,
+            }
+          };
+          const existingPlayer = await PlayerStat.findOne({ player_id: player.player_id });
+          if (existingPlayer) {
             existingPlayer.set(player);
             await existingPlayer.save();
-            console.log(`Player ${player.name} stats updated successfully in the database`);
-        } else {
+            console.log(`✅ Player ${player.name} stats updated`);
+          } else {
             await PlayerStat.create(player);
-            console.log(`Player ${player.name} stats saved successfully to the database`);
+            console.log(`🆕 Player ${player.name} stats saved`);
+          }
+          // delete the players from database if they are not in the members list
+          try {
+            const allPlayers = await PlayerStat.find();
+            const playersToDelete = allPlayers.filter((player: any) => !membersWithProfileLink.some((member: any) => member.player_id === player.player_id));
+            for (const player of playersToDelete) {
+              await PlayerStat.deleteOne({ _id: player._id });
+              console.log(`❌ Player ${player.name} deleted`);
+            }
+          }
+          catch (deleteError) {
+            console.error(`❌ Error deleting player ${membersWithProfileLink[i].name}:`, deleteError);
+          }
+          
+        } catch (playerError) {
+          console.error(`❌ Error scraping player ${membersWithProfileLink[i].name}:`, playerError);
         }
       }
+
       await StatMeta.deleteMany();
       await StatMeta.create({ lastUpdated: new Date() })
-      console.log('Player stats created successfully')
-     
-  
+      console.log('Players stats created successfully')
     } catch (error) {
       console.error('Error:', error);
     } finally {
--- src/models/PlayerStat.ts
+++ src/models/PlayerStat.ts
@@ -16,21 +16,51 @@ export interface IPlayerStat extends Document {
     bowler_category_info: string;
     profileLink: string;
     stats: {
-        Matches: string;
-        Innings: string;
-        'Not out': string;
-        Runs: string;
-        'Highest Runs': string;
-        Avg: string;
-        SR: string;
-        '30s': string;
-        '50s': string;
-        '100s': string;
-        '4s': string;
-        '6s': string;
-        Ducks: string;
-        Won: string;
-        Loss: string;
+        batting: {
+            Matches: string;
+            Innings: string;
+            'Not out': string;
+            Runs: string;
+            'Highest Runs': string;
+            Avg: string;
+            SR: string;
+            '30s': string;
+            '50s': string;
+            '100s': string;
+            '4s': string;
+            '6s': string;
+            Ducks: string;
+            Won: string;
+            Loss: string;
+        },
+        bowling: {
+            Matches: string;
+            Innings: string;
+            Overs: string;
+            Maidens: string;
+            Wickets: string;
+            Runs: string;
+            'Best Bowling': string;
+            '3 Wickets': string;
+            '5 Wickets': string;
+            Economy: string;
+            SR: string;
+            Avg: string;
+            Wides: string;
+            NoBalls: string;
+            'Dot Balls': string;
+            '4s': string;
+            '6s': string;
+        }, 
+        fielding: {
+            Matches: string;
+            Catches: string;
+            'Caught behind': string;
+            'Run outs': string;
+            Stumpings: string;
+            'Assisted Run Outs': string;
+            'Bye Runs (WK)': string;
+        }
     };
     }
 const PlayerStatSchema: Schema = new Schema({
@@ -94,68 +124,207 @@ const PlayerStatSchema: Schema = new Schema({
         type: String,
         required: true,
     },
+    //     Matches: {
+    //         type: String,
+    //         // required: true,
+    //     },
+    //     Innings: {
+    //         type: String,
+    //         // required: true,
+    //     },
+    //     'Not out': {
+    //         type: String,
+    //         // required: true,
+    //     },
+    //     Runs: {
+    //         type: String,
+    //         // required: true,
+    //     },
+    //     'Highest Runs': {
+    //         type: String,
+    //         // required: true,
+    //     },
+    //     Avg: {
+    //         type: String,
+    //         // required: true,
+    //     },
+    //     SR: {
+    //         type: String,
+    //         // required: true,
+    //     },
+    //     '30s': {
+    //         type: String,
+    //         // required: true,
+    //     },
+    //     '50s': {
+    //         type: String,
+    //         // required: true,
+    //     },
+    //     '100s': {
+    //         type: String,
+    //         // required: true,
+    //     },
+    //     '4s': {
+    //         type: String,
+    //         // required: true,
+    //     },
+    //     '6s': {
+    //         type: String,
+    //         // required: true,
+    //     },
+    //     Ducks: {
+    //         type: String,
+    //         // required: true,
+    //     },
+    //     Won: {
+    //         type: String,
+    //         // required: true,
+    //     },
+    //     Loss: {
+    //         type: String,
+    //         // required: true,
+    //     },
+    // },
     stats: {
-        Matches: {
-            type: String,
-            // required: true,
+        batting: {
+            Matches: {
+                type: String,
+                // required: true,
+            },
+            Innings: {
+                type: String,
+                // required: true,
+            },
+            'Not out': {
+                type: String,
+                // required: true,
+            },
+            Runs: {
+                type: String,
+                // required: true,
+            },
+            'Highest Runs': {
+                type: String,
+                // required: true,
+            },
+            Avg: {
+                type: String,
+                // required: true,
+            },
+            SR: {
+                type: String,
+                // required: true,
+            },
+            '30s': {
+                type: String,
+                // required: true,
+            },
+            '50s': {
+                type: String,
+                // required: true,
+            },
+            '100s': {
+                type: String,
+                // required: true,
+            },
+            '4s': {
+                type: String,
+                // required: true,
+            },
+            '6s': {
+                type: String,
+                // required: true,
+            },
+            Ducks: {
+                type: String,
+                // required: true,
+            },
+            Won: {
+                type: String,
+                // required: true,
+            },
+            Loss: {
+                type: String,
+                // required: true,
+            }
         },
-        Innings: {
-            type: String,
-            // required: true,
-        },
-        'Not out': {
-            type: String,
-            // required: true,
-        },
-        Runs: {
-            type: String,
-            // required: true,
-        },
-        'Highest Runs': {
-            type: String,
-            // required: true,
-        },
-        Avg: {
-            type: String,
-            // required: true,
-        },
-        SR: {
-            type: String,
-            // required: true,
-        },
-        '30s': {
-            type: String,
-            // required: true,
-        },
-        '50s': {
-            type: String,
-            // required: true,
-        },
-        '100s': {
-            type: String,
-            // required: true,
-        },
-        '4s': {
-            type: String,
-            // required: true,
-        },
-        '6s': {
-            type: String,
-            // required: true,
-        },
-        Ducks: {
-            type: String,
-            // required: true,
-        },
-        Won: {
-            type: String,
-            // required: true,
-        },
-        Loss: {
-            type: String,
-            // required: true,
-        },
+        bowling:{
+        Matches:{
+        type:String
+        }, 
+        Innings:{
+        type:String
+        }, 
+        Overs:{
+        type:String
+        }, 
+        Maidens:{
+        type:String
+        }, 
+        Wickets:{
+        type:String
+        }, 
+        Runs:{
+        type:String
+        }, 
+        "Best Bowling":{
+        type:String
+        }, 
+        "3 Wickets":{
+        type:String
+        }, 
+        "5 Wickets":{
+        type:String
+        }, 
+        Economy:{
+        type:String
+        }, 
+        SR:{
+        type:String
+        }, 
+        Avg:{
+        type:String
+        }, 
+        Wides:{
+        type:String
+        }, 
+         NoBalls:{
+         type:String
+         }, 
+         "Dot Balls":{
+         type:String
+         }, 
+         "4s":{
+         type:String
+         }, 
+         "6s":{
+         type:String
+         }
     },
+    fielding:{
+        Matches:{
+        type:String
+        }, 
+        Catches:{
+        type:String
+        }, 
+        "Caught behind":{
+        type:String
+        }, 
+        "Run outs":{
+        type:String
+        }, 
+        Stumpings:{
+        type:String
+        }, 
+        "Assisted Run Outs":{
+        type:String
+        }, 
+         "Bye Runs (WK)":{
+         type:String
+         }
+    }
+    }
 });
 
 const PlayerStat = mongoose.model<IPlayerStat>("PlayerStat", PlayerStatSchema);
